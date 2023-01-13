f4450d65b019190d2fe8c15c5054ba5bfb7379b0
eunji <qkrdmswl1018@naver.com>
2023-01-13 12:29:54 +0900

    Add styles

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,4 @@
 import User from "../models/User";
-import Video from "../models/Video";
 import fetch from "node-fetch";
 import bcrypt from "bcrypt";
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
@@ -196,9 +195,18 @@ export const postChangePassword = async (req, res) => {
 }
 export const see = async (req, res) => {
   const {id} = req.params;
-  const user = await User.findById(id).populate("videos");
-  if(!user){
-    return res.status("404").render("404", {pageTitle: "User not found"});
+  const user = await User.findById(id).populate({
+    path: "videos",
+    populate: {
+      path: "owner",
+      model: "User",
+    },
+  });
+  if (!user) {
+    return res.status(404).render("404", { pageTitle: "User not found." });
   }
-  return res.render("users/profile", {pageTitle: user.name, user});
-}
+ return res.render("users/profile", {
+   pageTitle: user.name,
+   user,
+ });
+};
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,8 +2,10 @@ import Video from "../models/Video";
 import User from "../models/User";
 
 export const home = async (req, res) => {
-  const videos = await Video.find({}).sort({ createdAt: "desc" });
-  return res.render("home", { pageTitle: "Home", videos });
+  const videos = await Video.find({})
+  .sort({ createdAt: "desc" })
+  .populate("owner");
+return res.render("home", { pageTitle: "Home", videos });
 };
 
 export const watch = async (req, res) => {
@@ -102,7 +104,7 @@ export const search = async (req, res) => {
       title: {
         $regex: new RegExp(`${keyword}$`, "i"),
       },
-    });
+    }).populate("owner");
   }
   return res.render("search", { pageTitle: "Search", videos });
 };
